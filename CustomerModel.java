a4704bf9c358b05065f022a6259c3b7880e77393
Hasan Kamal <hasankamal007@gmail.com>
2017-04-13 18:58:32 +0530

    database models added

---
+++ CustomerModel.java
@@ -0,0 +1,88 @@
+import javax.swing.table.*;
+import java.sql.*;
+
+public class CustomerModel extends AbstractTableModel{
+	
+	//Schema for Customer relation:
+	//	Customer(c_id, first_name, last_name, street, city, state, zip, date_birth, m_code, m_start_date)
+	//  insert into Customer values(1, 'Jeff', 'Bezos', 'Some Amazon Street', 'New York City', 'New York', 910191, '01-01-1970', 1, '01-01-1970');
+	//  create table Customer( c_id int, first_name varchar(50), last_name varchar(50), street varchar(50), city varchar(50), state varchar(50), zip int, date_birth varchar(50), m_code int, m_start_date varchar(50), primary key(c_id));
+
+	int rowCount;
+	String data[][];
+
+	public CustomerModel(){
+		try{
+			
+			Class.forName ("com.mysql.jdbc.Driver");
+			//Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@db.yale.edu:1521:univdb",userid, passwd);
+			String url = "jdbc:" + "mysql" + "://" + "localhost" + ":" + "3306" + "/" + "gym" + "?autoReconnect=true&useSSL=false";
+			String userid = "root";
+			String passwd = "hasankamal";
+
+			Connection conn = DriverManager.getConnection(url, userid, passwd);
+ 			
+ 			Statement stmt = conn.createStatement();
+			ResultSet rset = stmt.executeQuery( "select * from Customer"); 
+			
+			rowCount=0;
+			while(rset.next()){
+				rowCount++;
+			}
+			
+			rset = stmt.executeQuery( "select * from Customer");
+			data = new String[rowCount][10];
+			int i=0;
+			while (rset.next()) {
+				for(int j=0; j<10; j++)
+					data[i][j] = rset.getString(j+1);
+				i++;
+			}
+			stmt.close();
+			conn.close();
+
+		}catch(Exception sqle){
+			System.out.println("Exception: " + sqle);
+		}
+	}
+
+	public int getRowCount(){
+		return rowCount;
+	}
+
+  	public int getColumnCount(){
+  		return 10;
+  	}
+
+  	public Object getValueAt(int row, int column){
+  		return data[row][column];
+  	}
+
+  	public String getColumnName(int column){
+  		switch(column){
+  			case 0:
+  				return "c_id";
+  			case 1:
+  				return "first_name";
+  			case 2:
+  				return "last_name";
+  			case 3:
+  				return "street";
+  			case 4:
+  				return "city";
+  			case 5:
+  				return "state";
+  			case 6:
+  				return "zip";
+  			case 7:
+  				return "date_birth";
+  			case 8:
+  				return "m_code";
+  			case 9:
+  				return "m_start_date";
+  			default:
+  				return "-";
+  		}
+  	}
+
+}
--- GymManager.java
+++ GymManager.java
@@ -1,3 +1,171 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+
 public class GymManager{
-	
+
+	JFrame mainFrame;
+	JTable table;
+	JButton buttonLeft, buttonRight, buttonModify, buttonDelete;
+	int displayMode; //0-Customer, 1-Staff, 2-Equipment, 3-Membership
+
+	public GymManager(){
+
+		//outer enclosing JPanel
+		JPanel parentPanel = new JPanel();
+		parentPanel.setLayout(new BorderLayout());
+
+		//NORTH
+		{
+			JPanel topPart = new JPanel();
+			topPart.setLayout(new BorderLayout());
+			topPart.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.black));
+			
+			JLabel appTitle = new JLabel("Gym Management");
+			appTitle.setFont(appTitle.getFont().deriveFont(20.0f));
+			appTitle.setBorder(new EmptyBorder(20, 20, 20, 20));
+			//appTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+			topPart.add(appTitle, BorderLayout.CENTER);
+
+			parentPanel.add(topPart, BorderLayout.NORTH);
+		}
+
+		//CENTER
+		{
+			JPanel centerPanel = new JPanel();
+			centerPanel.setLayout(new BorderLayout());
+			centerPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
+
+			//First row of buttons
+			{
+				GridLayout buttonsInRowLayout = new GridLayout(1, 4);
+				JPanel buttonList = new JPanel();
+				buttonList.setLayout(buttonsInRowLayout);
+
+				JButton buttonCustomer = new JButton("Customer");
+				buttonCustomer.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						applyCustomerModel();
+					}
+				});
+				buttonList.add(buttonCustomer);
+
+				JButton buttonStaff = new JButton("Staff");
+				buttonStaff.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						applyStaffModel();
+					}
+				});
+				buttonList.add(buttonStaff);
+
+				JButton buttonEquipment = new JButton("Equipment");
+				buttonEquipment.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						applyEquipmentModel();
+					}
+				});
+				buttonList.add(buttonEquipment);
+
+				JButton buttonMembership = new JButton("Membership");
+				buttonMembership.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						applyMembershipModel();
+					}
+				});
+				buttonList.add(buttonMembership);
+				centerPanel.add(buttonList, BorderLayout.NORTH);
+			}
+
+			//table
+			{
+				table = new JTable();
+				applyCustomerModel();
+				JScrollPane scrollPane = new JScrollPane(table);
+				table.setFillsViewportHeight(true);
+				centerPanel.add(scrollPane, BorderLayout.CENTER);
+			}
+
+			//Second row of buttons
+			{
+				GridLayout buttonsInRowLayout = new GridLayout(1, 10);
+				JPanel buttonList = new JPanel();
+				buttonList.setLayout(buttonsInRowLayout);
+
+				buttonLeft = new JButton("<-");
+				buttonLeft.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+
+					}
+				});
+				buttonList.add(buttonLeft);
+
+				buttonRight = new JButton("->");
+				buttonRight.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+
+					}
+				});
+				buttonList.add(buttonRight);
+
+				buttonModify = new JButton("Modify");
+				buttonModify.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+
+					}
+				});
+				buttonList.add(buttonModify);
+
+				buttonDelete = new JButton("Delete");
+				buttonDelete.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+
+					}
+				});
+				buttonList.add(buttonDelete);
+
+				for(int i=0; i<4; i++){
+					JButton dummyButton = new JButton();
+					dummyButton.setVisible(false);
+					buttonList.add(dummyButton);
+				}
+
+				centerPanel.add(buttonList, BorderLayout.SOUTH);
+			}
+
+			parentPanel.add(centerPanel, BorderLayout.CENTER);
+		}
+
+		//main JFrame
+		mainFrame = new JFrame();
+		mainFrame.getContentPane().add(parentPanel);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setSize(900, 700);
+		mainFrame.setVisible(true);
+	}
+
+	public void applyCustomerModel(){
+		table.setModel(new CustomerModel());
+		displayMode = 0;
+	}
+
+	public void applyStaffModel(){
+		table.setModel(new StaffModel());
+		displayMode = 1;
+	}
+
+	public void applyEquipmentModel(){
+		table.setModel(new EquipmentModel());
+		displayMode = 2;
+	}
+
+	public void applyMembershipModel(){
+		table.setModel(new MembershipModel());
+		displayMode = 3;
+	}
+
+	public static void main(String[] args){
+		GymManager manager = new GymManager();
+	}
+
 }
